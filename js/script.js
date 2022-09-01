5bc0be0cf955bc5ea560f972fa736347d022a650
serhii-sultanov <sshsultanov@gmail.com>
2022-09-01 17:24:05 +0300

    added fetch and find  method

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((result) => {
+    let user = result.find((user) => user.company.name === "Johns Group");
+    console.log(user);
+  })
+  .catch((error) => console.error(error));
